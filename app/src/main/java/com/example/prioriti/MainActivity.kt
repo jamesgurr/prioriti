74c7a6de86bcda8e0cd770d8a2adb699c83214c5
jamesgurr <j2000492@byui.edu>
2025-02-11 18:27:29 -0700

    Added CreateTask
    Added a create task function that takes a date and a task name. It then displays it on the screen with a checkbox.

--- app/src/main/java/com/example/prioriti/MainActivity.kt
+++ app/src/main/java/com/example/prioriti/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.prioriti.ui.theme.PrioritiTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +28,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PrioritiTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Column(modifier = Modifier.padding(24.dp)) {
+                    CreateTask("Clean up trash", "2/12/24")
                 }
             }
         }
@@ -31,17 +37,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun CreateTask(name: String, date: String) {
+    /* Create a task with the given name and date
 
-@Preview(showBackground = true)
+    Parameters:
+    name (String): The name of the task
+    date (String): The date or time limit for the task if provided
+
+    Returns: None
+     */
+    var checked by remember { mutableStateOf(false) }
+    Column {
+        Row(verticalAlignment = Alignment.CenterVertically){
+            Checkbox(
+                checked = checked,
+                onCheckedChange = {checked = it}
+            )
+            Text(
+                text = name,
+                style = if(checked)TextStyle(textDecoration = TextDecoration.LineThrough)else TextStyle(textDecoration = null)
+            )
+        }
+        Text(
+            text = date,
+            modifier = Modifier.padding(start = 50.dp),
+            style = if(checked)TextStyle(textDecoration = TextDecoration.LineThrough)else TextStyle(textDecoration = null)
+        )
+    }
+}
+@Preview
 @Composable
-fun GreetingPreview() {
+fun CreateTaskPreview() {
     PrioritiTheme {
-        Greeting("Android")
+        CreateTask("Task", "Date")
     }
 }
